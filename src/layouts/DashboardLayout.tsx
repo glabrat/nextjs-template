7c55bf5f319af0e99782ed5855d92b21f1be7f82
Eduardo Reyes <ereyesoyarzo@gmail.com>
2022-01-24 14:19:45 -0300

    refactor(sidebar): new margins and paddings

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -16,26 +16,26 @@ const DashboardLayout: React.FC = ({ children }) => {
 
   return (
     <Flex height={'100vh'} width={'100%'} bg="gray.50">
-      <Flex py={4} px={8} direction={'column'} width={72} overflowY="auto">
-        <Heading p={3} mb={4} as="h2" variant={'sidebar'}>
+      <Flex px={8} direction={'column'} minW={72} overflowY="auto">
+        <Heading my={8} as="h2" variant={'sidebar'}>
           Rokket UI Dashboard
         </Heading>
         <ButtonSidebar
-          mb={3}
+          mb={2}
           onClick={() => setIndex(0)}
           variant={index === 0 ? 'active' : ''}
           icon={IoHomeOutline}
           text="Dashboard"
         />
         <ButtonSidebar
-          mb={3}
+          mb={2}
           variant={index === 1 ? 'active' : ''}
           onClick={() => setIndex(1)}
           icon={IoStatsChartOutline}
           text="Tables"
         />
         <ButtonSidebar
-          mb={3}
+          mb={2}
           variant={index === 2 ? 'active' : ''}
           onClick={() => setIndex(2)}
           icon={IoWalletOutline}
@@ -43,25 +43,24 @@ const DashboardLayout: React.FC = ({ children }) => {
         />
 
         <ButtonSidebar
-          mb={3}
+          mb={2}
           variant={index === 3 ? 'active' : ''}
           onClick={() => setIndex(3)}
           icon={IoBuildOutline}
           text="RTL"
         />
-        <Heading p={3} my={4} as="h2" variant={'sidebar'}>
+        <Heading my={8} as="h2" variant={'sidebar'}>
           Account pages
         </Heading>
 
         <ButtonSidebar
-          mb={3}
+          mb={2}
           variant={index === 4 ? 'active' : ''}
           onClick={() => setIndex(4)}
           icon={IoPersonOutline}
           text="Profile"
         />
         <ButtonSidebar
-          mb={3}
           variant={index === 5 ? 'active' : ''}
           onClick={() => setIndex(5)}
           icon={IoDocumentOutline}
