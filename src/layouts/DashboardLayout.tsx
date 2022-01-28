e9fc7b0fae8a398bedd0f978bb5fd6c0a78c404d
Eduardo Reyes <ereyesoyarzo@gmail.com>
2022-01-28 15:51:13 -0300

    feature(dashboard): first layout implementation without collapse menu

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,21 +1,83 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { BiUser } from 'react-icons/bi'
+import { BsBuilding } from 'react-icons/bs'
+import { FaMinusSquare } from 'react-icons/fa'
+import { FiBriefcase, FiRadio } from 'react-icons/fi'
+import {
+  MdMenu,
+  MdOutlineHelpOutline,
+  MdOutlineLogout,
+  MdOutlineSettings,
+} from 'react-icons/md'
+import { VscBell } from 'react-icons/vsc'
+import { Divider, Flex, Image, VStack } from '@chakra-ui/react'
 
-import HeaderDashboard from 'components/HeaderDashboard'
-import { Sidebar } from 'components/Sidebar'
+import { TooltipDashboard } from 'components/Dashboard/TooltipDashboard'
 
 const DashboardLayout: React.FC = ({ children }) => {
   return (
-    <Flex height={'100vh'} width={'100%'} bg="gray.50">
-      <Box display={{ base: 'none', md: 'flex' }}>
-        <Sidebar />
-      </Box>
+    <Flex height={'100vh'} width={'100%'} bg="#F7FAFC">
+      <Flex
+        direction={'column'}
+        align={'center'}
+        justify={'space-between'}
+        minW={16}
+        bg="#FFFFFF"
+        p={2}
+      >
+        <VStack mt={2} spacing={4} direction={'column'}>
+          <TooltipDashboard placement="right" label="Menú" icon={MdMenu} />
+          <TooltipDashboard
+            placement="right"
+            label="Locales"
+            icon={BsBuilding}
+          />
+          <TooltipDashboard
+            placement="right"
+            label="Tiempo"
+            icon={FaMinusSquare}
+          />
+          <TooltipDashboard
+            placement="right"
+            label="Visualización"
+            icon={FiBriefcase}
+          />
 
-      <Flex direction={'column'} width={'100%'} overflow={'auto'}>
-        <HeaderDashboard />
+          <TooltipDashboard
+            placement="right"
+            label="Cobertura"
+            icon={FiRadio}
+          />
+        </VStack>
 
-        <Box mx={{ lg: 8 }} m={{ base: 2, md: 0 }}>
-          {children}
-        </Box>
+        <VStack spacing={4} mb={2} direction="column">
+          <Divider size="2"></Divider>
+          <TooltipDashboard
+            placement="right"
+            label="Configuración"
+            icon={MdOutlineSettings}
+          />
+          <TooltipDashboard
+            placement="right"
+            label="Ayuda"
+            icon={MdOutlineHelpOutline}
+          />
+        </VStack>
+      </Flex>
+      <Flex w="full" direction={'column'} overflow={'auto'}>
+        <Flex
+          minH={'4.5rem'}
+          bg="#FFFFFF"
+          justify="space-between"
+          align={'center'}
+        >
+          <Image ml={10} src="/rokketlabs.svg" height={51} width={143} />
+          <Flex mr={24} w={28} justify={'space-between'} align="center">
+            <TooltipDashboard label="Mi Perfil" icon={BiUser} />
+            <TooltipDashboard label="Notificaciones" icon={VscBell} />
+            <TooltipDashboard label="Salir" icon={MdOutlineLogout} />
+          </Flex>
+        </Flex>
+        <Flex direction={'column'}>{children}</Flex>
       </Flex>
     </Flex>
   )
---
+++ src/theme/components/tooltip.ts
@@ -0,0 +1,9 @@
+export const tooltipStyles = {
+  Tooltip: {
+    baseStyle: {
+      bg: '#0C3C6B',
+      borderRadius: 'md',
+      fontSize: 'xs',
+    },
+  },
+}
