89a2a86446bf61b23af2ec18aea4e6f912a4c87b
Arturo Salinas <arturo.salinas@rokketlabs.com>
2022-02-01 16:02:21 -0300

    feat: add logout handling to exit button

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react'
 import { BiUser } from 'react-icons/bi'
 import { MdOutlineLogout } from 'react-icons/md'
 import { VscBell } from 'react-icons/vsc'
 import { Flex, Image } from '@chakra-ui/react'
+import { AuthContext } from 'context/AuthContext'
 
 import { TooltipDashboard } from 'components/Dashboard/TooltipDashboard'
 import { Sidebar } from 'components/Sidebar'
 
 const DashboardLayout: React.FC = ({ children }) => {
+  const { handleLogout } = useContext(AuthContext)
+
   return (
     <Flex height={'100vh'} width={'100%'} bg="#F7FAFC">
       <Sidebar />
@@ -21,7 +25,11 @@ const DashboardLayout: React.FC = ({ children }) => {
           <Flex mr={24} w={28} justify={'space-between'} align="center">
             <TooltipDashboard label="Mi Perfil" icon={BiUser} />
             <TooltipDashboard label="Notificaciones" icon={VscBell} />
-            <TooltipDashboard label="Salir" icon={MdOutlineLogout} />
+            <TooltipDashboard
+              label="Salir"
+              icon={MdOutlineLogout}
+              handleClick={handleLogout}
+            />
           </Flex>
         </Flex>
         <Flex direction={'column'}>{children}</Flex>
